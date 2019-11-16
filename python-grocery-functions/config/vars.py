666ebe285344d88f814bf284bf27a87071248752
Viktor Andersen <vikfand@gmail.com>
2019-11-16 14:43:58 +0800

    use load_dotenv

--- python-grocery-functions/config/vars.py
+++ python-grocery-functions/config/vars.py
@@ -1,5 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=".env.production")
+
+
 MONGO_HOST = os.getenv('MONGO_HOST', '0.0.0.0')
 MONGO_PORT = os.getenv('MONGO_PORT', '27017')
 MONGO_USER = os.getenv('MONGO_USER', '')
