b88f069ea250e50f6118ab693ef82f6ae26944e3
Viktor Andersen <vikfand@gmail.com>
2020-02-12 16:11:18 +0100

    Publish to SNS topic when products are handled.

--- python-grocery-functions/scraper_feed/handle_feed.py
+++ python-grocery-functions/scraper_feed/handle_feed.py
@@ -1,6 +1,12 @@
+import boto3
+import json
+
 from scraper_feed.handle_products import handle_products
 from storage.db import save_scraped_products, get_handle_config
+from util.helpers import json_handler
 
+
+from config.vars import SCRAPER_FEED_HANDLED_TOPIC_ARN
 from util.enums import provenances
 from util.errors import NoHandleConfigError
 
@@ -28,8 +34,27 @@ def handle_feed(feed: list, config: dict) -> dict:
     Handles a feed from Scrapy according to the provided config.
     """
 
+    sns_client = boto3.client("sns")
+
     _config = _get_handle_config(config)
 
     products = handle_products(feed, _config)
+    products = list(
+        {**product, "siteCollection": _config["collection_name"]}
+        for product in products
+    )
     result = save_scraped_products(products, _config["collection_name"])
+    # To allow event-driven behaviour, we publish an sns topic when products are saved successfully.
+    sns_message_data = {
+        **config,
+        "collection_name": _config["collection_name"],
+    }
+    sns_message = json.dumps(
+        {"default": json.dumps(sns_message_data, default=json_handler)}
+    )
+    sns_client.publish(
+        Message=sns_message,
+        MessageStructure="json",
+        TargetArn=SCRAPER_FEED_HANDLED_TOPIC_ARN,
+    )
     return result
