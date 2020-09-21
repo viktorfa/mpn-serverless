cdff2548ba5bd7c52a437471302546af2b0c3146
Viktor Andersen <vikfand@gmail.com>
2020-09-21 18:48:18 +0200

    Use mongo uri in config

--- node-grocery-functions/config/mongo.js
+++ node-grocery-functions/config/mongo.js
@@ -1,20 +1,7 @@
 const mongodb = require("mongodb");
 
-const {
-  mongoHost,
-  mongoPort,
-  mongoUser,
-  mongoPassword,
-  mongoDatabase,
-} = require("./vars");
+const { mongoUri, mongoDatabase } = require("./vars");
 
-const getMongoUri = () => {
-  if (!mongoHost || !mongoPort) throw "No mongo host";
-  else if (!!mongoUser && mongoUser !== "undefined")
-    return `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}`;
-  else return `mongodb://${mongoHost}:${mongoPort}`;
-};
-const mongoUri = getMongoUri();
 console.log(`Connecting to: ${mongoUri}`);
 const client = new mongodb.MongoClient(mongoUri, { useUnifiedTopology: true });
 
--- node-grocery-functions/config/vars.js
+++ node-grocery-functions/config/vars.js
@@ -1,8 +1,5 @@
 module.exports = {
-  mongoHost: process.env.MONGO_HOST,
-  mongoPort: process.env.MONGO_PORT,
-  mongoUser: process.env.MONGO_USER,
-  mongoPassword: process.env.MONGO_PASSWORD,
+  mongoUri: process.env.MONGO_URI,
   mongoDatabase: process.env.MONGO_DATABASE,
   bucketName: process.env.BUCKET_NAME,
   cloudFrontDistributionId: process.env.CLOUD_FRONT_DISTRIBUTION_ID,
