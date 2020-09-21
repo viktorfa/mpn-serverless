cd120ae2c80d419db03df60a966b0c1b36501141
Viktor Andersen <vikfand@gmail.com>
2020-09-21 20:04:50 +0200

    Use mongo uri in config

--- python-ml/config/mongo.py
+++ python-ml/config/mongo.py
@@ -1,19 +1,8 @@
 import pymongo
 
-from config.vars import (
-    MONGO_HOST,
-    MONGO_PORT,
-    MONGO_USER,
-    MONGO_PASSWORD,
-    MONGO_DATABASE,
-)
+from config.vars import MONGO_URI, MONGO_DATABASE
 
-if MONGO_USER:
-    _client = pymongo.MongoClient(
-        MONGO_HOST, int(MONGO_PORT), username=MONGO_USER, password=MONGO_PASSWORD
-    )
-else:
-    _client = pymongo.MongoClient(MONGO_HOST, int(MONGO_PORT))
+_client = pymongo.MongoClient(MONGO_URI)
 
 _db = _client[MONGO_DATABASE]
 
