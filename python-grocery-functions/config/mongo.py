074dd7d915edf6b08c67cdaefba96424394812b8
Viktor Andersen <vikfand@gmail.com>
2020-09-21 19:21:02 +0200

    Use mongo uri in config

--- python-grocery-functions/config/mongo.py
+++ python-grocery-functions/config/mongo.py
@@ -1,22 +1,11 @@
 import pymongo
 
-from config.vars import (
-    MONGO_HOST,
-    MONGO_PORT,
-    MONGO_USER,
-    MONGO_PASSWORD,
-    MONGO_DATABASE,
-)
+from config.vars import MONGO_URI, MONGO_DATABASE
 
-if MONGO_USER:
-    _client = pymongo.MongoClient(
-        MONGO_HOST, int(MONGO_PORT), username=MONGO_USER, password=MONGO_PASSWORD
-    )
-else:
-    _client = pymongo.MongoClient(MONGO_HOST, int(MONGO_PORT))
+_client = pymongo.MongoClient(MONGO_URI)
 
-_db = _client.get_database(MONGO_DATABASE)
+_db = _client[MONGO_DATABASE]
 
 
-def get_collection(collection_name: str) -> pymongo.collection:
-    return _db.get_collection(collection_name)
+def get_collection(collection_name: str):
+    return _db[collection_name]
--- python-grocery-functions/config/vars.py
+++ python-grocery-functions/config/vars.py
@@ -5,9 +5,6 @@ from dotenv import load_dotenv
 dotenv_path = ".env.production" if os.getenv("STAGE") == "prod" else ".env.development"
 load_dotenv(dotenv_path=dotenv_path)
 
-MONGO_HOST = os.getenv("MONGO_HOST", "0.0.0.0")
-MONGO_PORT = os.getenv("MONGO_PORT", "27017")
-MONGO_USER = os.getenv("MONGO_USER", "")
-MONGO_PASSWORD = os.getenv("MONGO_PASSWORD", "")
+MONGO_URI = os.environ["MONGO_URI"]
 MONGO_DATABASE = os.environ["MONGO_DATABASE"]
 SCRAPER_FEED_HANDLED_TOPIC_ARN = os.environ["SCRAPER_FEED_HANDLED_TOPIC_ARN"]
