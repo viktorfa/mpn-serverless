df2482a25529718b8bf9bafb3bb91c386567c49e
Viktor Andersen <vikfand@gmail.com>
2021-05-02 04:35:28 +0200

    Add book handler

--- python-grocery-functions/book_feed/handler.py
+++ python-grocery-functions/book_feed/handler.py
@@ -1,7 +1,47 @@
+import os
 import json
+import logging
 
-from book_feed.process_books import process_books
+
+import sentry_sdk
+from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
+
+from book_feed.process_books import process_books, merge_similar_books
+
+
+if not os.getenv("IS_LOCAL"):
+    sentry_sdk.init(
+        integrations=[AwsLambdaIntegration()],
+    )
+
+logger = logging.getLogger()
+logging.getLogger("botocore").setLevel(logging.WARNING)
+logging.getLogger("boto3").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+if os.getenv("IS_LOCAL"):
+    logging.basicConfig(level=logging.DEBUG)
+    logger.setLevel(logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
+    logger.setLevel(logging.INFO)
 
 
 def handle_book_offers_trigger(event, context):
-    return json.dumps(process_books(), default=str)
+    namespace = event.get("namespace")
+    offer_filter = {}
+    if namespace:
+        offer_filter["dealer"] = namespace
+    return json.dumps(process_books(offer_filter), default=str)
+
+
+def handle_book_offers_sns(event, context):
+    sns_message = json.loads(event["Records"][0]["Sns"]["Message"])
+    namespace = sns_message["namespace"]
+    offer_filter = {}
+    if namespace:
+        offer_filter["dealer"] = namespace
+    return json.dumps(process_books(offer_filter), default=str)
+
+
+def process_books_trigger(event, context):
+    return json.dumps(merge_similar_books(), default=str)
--- python-grocery-functions/book_feed/process_books.py
+++ python-grocery-functions/book_feed/process_books.py
@@ -1,19 +1,57 @@
 import logging
 from datetime import datetime
 import pydash
-from pymongo import UpdateOne, InsertOne
+from pymongo import UpdateOne, InsertOne, ASCENDING
+from bson import ObjectId
 
 from storage.db import get_collection
 
 
-def process_books():
+def merge_similar_books():
+    book_collection = get_collection("bookproducts")
+    all_books = book_collection.find(
+        {"mergedTo": None}, {"isbns": 1, "offers.author": 1, "offers.title": 1}
+    ).sort("_id", ASCENDING)
+
+    title_to_book_map = {}
+    updates = []
+
+    for book in all_books:
+        title = book["offers"][0]["title"]
+        existing_book = title_to_book_map.get(title)
+
+        if existing_book:
+            logging.info(f"Found existing book: {title}")
+            author = pydash.get(book, ["offers", 0, "author"])
+            existing_book_author = pydash.get(existing_book, ["offers", 0, "author"])
+            if author and author == existing_book_author:
+                logging.info(f"Merging book")
+                updates.append(
+                    UpdateOne(
+                        {"_id": ObjectId(book["_id"])},
+                        {"$set": {"mergedTo": ObjectId(existing_book["_id"])}},
+                    )
+                )
+                updates.append(
+                    UpdateOne(
+                        {"_id": ObjectId(existing_book["_id"])},
+                        {"$addToSet": {"isbns": {"$each": book["isbns"]}}},
+                    )
+                )
+        else:
+            title_to_book_map[title] = book
+
+    logging.debug(f"Writing {len(updates)} updates")
+
+    return book_collection.bulk_write(updates).bulk_api_result
+
+
+def process_books(offer_filter={}):
     now = datetime.now()
     offer_collection = get_collection("bookoffers")
     book_collection = get_collection("bookproducts")
     scraped_offers = offer_collection.find(
-        {
-            "validThrough": {"$gt": now},
-        },
+        {"validThrough": {"$gt": now}, **offer_filter},
         {
             "uri": 1,
             "provenance": 1,
@@ -22,19 +60,20 @@ def process_books():
             "price": 1,
             "href": 1,
             "title": 1,
+            "author": 1,
+            "book_type": 1,
         },
     )
 
     inserts = []
     updates = []
 
-    all_books = book_collection.find({})
+    all_books = book_collection.find({"mergedTo": None}, {"isbns": 1, "offers.uri": 1})
 
     isbn_to_book_map = {}
 
     for book in all_books:
-        isbn = pydash.get(book, ["gtins", "isbn13"])
-        if isbn:
+        for isbn in book["isbns"]:
             isbn_to_book_map[isbn] = book
 
     for offer in scraped_offers:
@@ -42,31 +81,48 @@ def process_books():
         if not offer_gtin:
             continue
         existing_book = isbn_to_book_map.get(offer_gtin)
-        book_offer_key = offer["uri"]
         book_offer = {
             "price": offer.get("price"),
             "href": offer["href"],
             "uri": offer["uri"],
             "dealer": offer["dealer"],
             "title": offer["title"],
-            "author": offer["author"],
+            "author": offer.get("author", ""),
             "book_type": offer["book_type"],
             "updated": now,
         }
         if existing_book:
-            updates.append(
-                UpdateOne(
-                    {"gtins.isbn13": offer_gtin},
-                    {"$set": {f"offers.{book_offer_key}": book_offer}},
+            updated_book_offers = existing_book["offers"]
+            if next(
+                (x for x in existing_book["offers"] if x["uri"] == offer["uri"]),
+                None,
+            ):
+                updates.append(
+                    UpdateOne(
+                        {"isbns": offer_gtin, "mergedTo": None},
+                        {"$set": {"offers.$[elem]": book_offer}},
+                        upsert=False,
+                        array_filters=[{"elem.uri": offer["uri"]}],
+                    )
                 )
-            )
+            else:
+                updates.append(
+                    UpdateOne(
+                        {"isbns": offer_gtin, "mergedTo": None},
+                        {"$push": {"offers": book_offer}},
+                    )
+                )
+
         else:
             new_book = {
                 "title": offer["title"],
                 "gtins": offer["gtins"],
-                "offers": {book_offer_key: book_offer},
+                "offers": [book_offer],
+                "isbns": [offer_gtin],
+                "mergedTo": None,
             }
             inserts.append(InsertOne(new_book))
+            isbn_to_book_map[offer_gtin] = new_book
 
     mongo_requests = [*inserts, *updates]
 
